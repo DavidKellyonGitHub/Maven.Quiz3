4e9ec380cab0c1bd8a716dd528ec02e95b71862e
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-03-09 12:28:56 -0400

    up to wave

--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -1,10 +1,26 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import rocks.zipcode.io.quiz3.fundamentals.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class WaveGenerator {
     public static String[] wave(String str) {
+//        ArrayList<String> waveList = new ArrayList<String>(Arrays.asList(str.toCharArray());
+//        String waveString = "";
+//        String lowerCase =
+//        for (String character : waveList){
+//            if (character != " "){
+//                StringUtils.capitalizeNthCharacter()
+//            }
+//        }
+//        for (int i = 0;i < str.length();i++){
+//            str.to
+//        }
         return null;
     }
 }
